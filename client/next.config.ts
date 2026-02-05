76d28a97c0ee11c89a313b38d7e6babe94155981
realmstoriches <facebookfortune84@gmail.com>
2026-02-05 18:52:57 -0500

    Fix: Added path aliases to tsconfig

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  /* Turbo is now handled automatically or at the top level in this version */
 };
 
 export default nextConfig;
